35ae6d05f0a43bc01290d323856b0b6a50d86339
OrlandoHsu <2858117894@qq.com>
2025-12-01 16:02:05 +0800

    去除快速启动文件

--- backend/start.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-echo 启动后端服务...
-echo 服务地址: http://localhost:5000
-echo 管理界面: http://localhost:5000/admin
-echo.
-cd /d "%~dp0"
-pixi run start
--- frontend/start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo 启动前端开发服务器...
-echo 服务地址: http://localhost:8080
-echo.
-cd /d "%~dp0"
-pixi run start
